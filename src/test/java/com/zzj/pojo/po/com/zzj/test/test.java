0d3cd3da49489164ce61c475e9dcb116368468f0
Zzzzzzzj <zzj550341954@gmail.com>
2018-01-05 20:08:19 +0800

    初始提交工程

---
+++ src/test/java/com/zzj/pojo/po/com/zzj/test/test.java
@@ -0,0 +1,18 @@
+package com.zzj.pojo.po.com.zzj.test;
+
+import org.junit.Test;
+
+import com.zzj.pojo.po.Student;
+
+public class test {
+
+	
+	@Test
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Student A=new Student();
+			A.setSid(1);
+			System.out.println(A);
+	}
+
+}
